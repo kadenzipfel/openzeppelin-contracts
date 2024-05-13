2ca5d15ce1f50a18bee02a9b4022b925e0ebf2b1
kaden <kaden.zipfel@hotmail.com>
2024-05-13 08:44:18 -0700

    Update ERC20Permit.sol

--- contracts/token/ERC20/extensions/ERC20Permit.sol
+++ contracts/token/ERC20/extensions/ERC20Permit.sol
@@ -36,7 +36,7 @@ abstract contract ERC20Permit is ERC20, IERC20Permit, EIP712, Nonces {
      *
      * It's a good idea to use the same `name` that is defined as the ERC-20 token name.
      */
-    constructor(string memory name) EIP712(name, "1") {}
+    constructor(string memory _name) EIP712(_name, "1") {}
 
     /**
      * @inheritdoc IERC20Permit
